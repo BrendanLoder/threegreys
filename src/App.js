afd72d37ca40cb5f56dac3e277da61e99a482618
Brendan Loder <brendanloder@Brendans-MBP.fios-router.home>
2022-05-09 16:41:25 -0400

    Changes For V2 Test 5

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2 Test 4</h1>
+        <h1>Hello from V2 Test 5</h1>
       </header>
     </div>
   );
