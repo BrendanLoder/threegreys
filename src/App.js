6e43eb420a8f447ece42c85e13ca5f460068c924
Brendan Loder <brendanloder@Brendans-MBP.fios-router.home>
2022-05-10 15:30:51 -0400

    removed auth but kept db

--- src/App.js
+++ src/App.js
@@ -43,38 +43,38 @@ const App = () => {
 
 
   return (
-    <Authenticator>
-      {({ signOut, user }) => (
-        <div style={styles.container}>
-          <h1>Hello {user.username}</h1>
-          <button style={styles.button} onClick={signOut}>Sign out</button>
-          <br />
-          <h2>Amplify Todos</h2>
-          <input
-            onChange={event => setInput('name', event.target.value)}
-            style={styles.input}
-            value={formState.name}
-            placeholder="Name"
-          />
-          <input
-            onChange={event => setInput('description', event.target.value)}
-            style={styles.input}
-            value={formState.description}
-            placeholder="Description"
-          />
-          <button style={styles.button} onClick={addTodo}>Create Todo</button>
-          {
-            todos.map((todo, index) => (
-              <div key={todo.id ? todo.id : index} style={styles.todo}>
-                <p style={styles.todoName}>{todo.name}</p>
-                <p style={styles.todoDescription}>{todo.description}</p>
-              </div>
-            ))
-          }
-        </div>
-      )}
-    </Authenticator>
-  );
+//   <Authenticator>
+//     {({ signOut, user }) => (
+      <div style={styles.container}>
+        {/* <h1>Hello {user.username}</h1>
+        <button style={styles.button} onClick={signOut}>Sign out</button>
+        <br /> */}
+        <h2>Amplify Todos</h2>
+        <input
+          onChange={event => setInput('name', event.target.value)}
+          style={styles.input}
+          value={formState.name}
+          placeholder="Name"
+        />
+        <input
+          onChange={event => setInput('description', event.target.value)}
+          style={styles.input}
+          value={formState.description}
+          placeholder="Description"
+        />
+        <button style={styles.button} onClick={addTodo}>Create Todo</button>
+        {
+          todos.map((todo, index) => (
+            <div key={todo.id ? todo.id : index} style={styles.todo}>
+              <p style={styles.todoName}>{todo.name}</p>
+              <p style={styles.todoDescription}>{todo.description}</p>
+            </div>
+          ))
+        }
+      </div>
+//     )}
+//   </Authenticator>
+);
 
 //   return (
 //     <div style={styles.container}>
