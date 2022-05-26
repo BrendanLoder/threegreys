1b101219fb63ec1b4d16f19d3b040343c6707afd
Brendan Loder <brendanloder@Brendans-MBP.fios-router.home>
2022-05-26 17:04:06 -0400

    updated app.js to include /lib/firebase and context/firebase

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { db } from './lib/firebase';
+import FirebaseContext from './context/firebase';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -7,15 +9,15 @@ import Amplify from 'aws-amplify';
 import config from './aws-exports';
 Amplify.configure(config);
 
+// const AWS = require('aws-sdk')
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <FirebaseContext.Provider value={{ db }}>
+            <App />
+        </FirebaseContext.Provider>
+    </React.StrictMode>
 );
 
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
