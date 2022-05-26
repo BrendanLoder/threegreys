a61e4f086fa8c0bb387e71f6bf16811d6c5103bc
Brendan Loder <brendanloder@Brendans-MBP.fios-router.home>
2022-05-26 16:55:06 -0400

    added /lib/firebase.js

---
+++ src/lib/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore/lite';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDGIScToAC88qff3hLDcfzQqBKNSBe-XLU",
+    authDomain: "threegrays-3cf6a.firebaseapp.com",
+    projectId: "threegrays-3cf6a",
+    storageBucket: "threegrays-3cf6a.appspot.com",
+    messagingSenderId: "32475921296",
+    appId: "1:32475921296:web:3bdf10bb9e5dc4cb119115"
+  };
+  
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export {db}
