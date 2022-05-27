94f81cea6bc110fa513bd1af8ef560134056e10d
Brendan Loder <brendanloder@Brendans-MBP.fios-router.home>
2022-05-27 15:53:50 -0400

    added swiper new settings and images

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,44 +1,105 @@
 import { useParams } from 'react-router-dom';
-import Swiper from 'swiper';
+
+
+
+// import Swiper, { Navigation, Pagination } from 'swiper';
+// // import Swiper and modules styles
+// import 'swiper/css';
+// import 'swiper/css/navigation';
+// import 'swiper/css/pagination';
+
+// import Swiper bundle with all modules installed
+import Swiper from 'swiper/bundle';
+
+// import styles bundle
+import 'swiper/css/bundle';
 
 function Profile({user}) {
 
 
+    console.log("Start")
+    // init Swiper:
     const swiper = new Swiper('.swiper', {
-        speed: 400,
-        spaceBetween: 100,
-    });
+        // Optional parameters
+        direction: 'horizontal',
+        loop: true,
+      
+        // If we need pagination
+        pagination: {
+          el: '.swiper-pagination',
+        },
+      
+        // Navigation arrows
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+      
+        // And if we need scrollbar
+        scrollbar: {
+          el: '.swiper-scrollbar',
+        },
+        autoHeight: true
+      });
 
-    // const swiperDiv = document.querySelector('.swiper').swiper;
-    // swiperDiv.allowSlideNext()
+      console.log(swiper)
 
-    return (
-        <div>
-            User is: {user.username}<br/><br/>
+return (
+    // START PAGE CONTAINER
+    <div>
 
-            {/* <!-- Slider main container --> */}
-            <div class="swiper">
-                {/* <!-- Additional required wrapper --> */}
-                <div class="swiper-wrapper">
-                    {/* <!-- Slides --> */}
-                    <div class="swiper-slide">Slide 1</div>
-                    <div class="swiper-slide">Slide 2</div>
-                    <div class="swiper-slide">Slide 3</div>
-                    {/* ... */}
-                </div>
-                {/* <!-- If we need pagination --> */}
-                <div class="swiper-pagination"></div>
+        <style>
+            {`
+            .swiper {
+                width: 600px;
+                height: 300px;
+            }
+            `}
+        </style>
 
-                {/* <!-- If we need navigation buttons --> */}
-                <div class="swiper-button-prev"></div>
-                <div class="swiper-button-next"></div>
+        User is: {user.username}<br/><br/>
 
-                {/* <!-- If we need scrollbar --> */}
-                <div class="swiper-scrollbar"></div>
+        START SWIPER:<br/><br/>
+
+
+        {/* <!-- Slider main container --> */}
+        <div className="swiper">
+            {/* <!-- Additional required wrapper --> */}
+            <div className="swiper-wrapper">
+            {/* <!-- Slides --> */}
+            <div className="swiper-slide">
+                <img src="/images/alice_1.jpg"/>
             </div>
-            
+            <div className="swiper-slide">
+                <img src="/images/all_grays_1.jpg"/>
+            </div>
+            <div className="swiper-slide">
+                <img src="/images/sam_1.jpg"/>
+            </div>
+            <div className="swiper-slide">
+                <img src="/images/sam_2.jpg"/>
+            </div>
+            <div className="swiper-slide">
+                <img src="/images/sly_1.jpg"/>
+            </div>
+            <div className="swiper-slide">
+                <img src="/images/sly_2.jpg"/>
+            </div>
+            </div>
+            {/* <!-- If we need pagination --> */}
+            <div className="swiper-pagination"></div>
+
+            {/* <!-- If we need navigation buttons --> */}
+            <div className="swiper-button-prev"></div>
+            <div className="swiper-button-next"></div>
+
+            {/* <!-- If we need scrollbar --> */}
+            <div className="swiper-scrollbar"></div>
         </div>
-    )
+        
+    {/* END PAGE CONTAINER */}
+    </div>
+)
 }
 
 export default Profile
