97082de8bff75f2f2f9042ff10e96d2499795f3d
Brendan Loder <brendanloder@Brendans-MBP.fios-router.home>
2022-06-06 14:09:57 -0400

    Added new carousel test images t3

--- src/components/carousel-type-2.js
+++ src/components/carousel-type-2.js
@@ -9,16 +9,16 @@ function CarouselType2 () {
             <h2>Type 2</h2>
             <Carousel showThumbs={false} infiniteLoop={true}>
                 <div style={{ height: "100%", width:"100%", color: "#fff" }}>
-                <img alt="" src="/images/t2.jpg" />
-                <p className="legend">Baz #1</p>
+                    <img alt="" src="/images/t2.jpg" />
+                    <p className="legend">Baz #1</p>
                 </div>
                 <div style={{ height: "100%", color: "#fff" }}>
-                <img alt="" src="/images/t3.jpg" />
-                <p className="legend">Bax #2</p>
+                    <img alt="" src="/images/t3.jpg" />
+                    <p className="legend">Bax #2</p>
                 </div>
                 <div style={{ height: "100%", color: "#fff" }}>
-                <img alt="" src="/images/t6.jpg" />
-                <p className="legend">William</p>
+                    <img alt="" src="/images/t6.jpg" />
+                    <p className="legend">William</p>
                 </div>
                 
             </Carousel>
