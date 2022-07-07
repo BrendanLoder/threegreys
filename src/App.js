136fe38418d260b2e68f8508f6238f2f072bfcb5
Brendan Loder <brendan.loder@gmail.com>
2022-07-07 10:22:49 -0400

    version 7.7.1 -  added hamburger, fixed footer

--- src/App.js
+++ src/App.js
@@ -59,57 +59,52 @@ const App = ({ signOut, user }) => {
 
     return (
         <div className="w-screen justify-center text-center p-0 m-auto h-screen">
-{/* <div class="max-w-4xl w-full"></div> */}
-            <div className="bg-gray-500 flex justify-center text-5xl w-screen font-bold text-white border-b-2 border-gray-600 shadow-lg">
-                <div className="w-30 h-30 pt-6 pr-2">
-                    Three
+            <div className="bg-gray-500 flex justify-center text-5xl w-screen font-bold text-white border-b-2 border-gray-600">
+                <div class="p-3 font-serif text-2xl border border-1 m-2 font-bold">
+                    Three Grays
                 </div>
-                <div className="w-20 h-21 rounded-full">
-                    <img src="/images/sly_face.jpg" class="w-20 h-20 rounded-full mx-auto border-2 border-gray-700 my-2" />
-                </div>
-                <div className="w-30 h-30 pt-6 pl-2">
-                    Grays
-                </div>
+            </div>
+
+            <div className="absolute top-5 right-3">
+                <button type="button" class="text-white hover:text-gray-300">
+                    <svg class="w-8 h-8 fill-current" viewBox="0 0 24 24"><path fill-rule="evenodd" d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"/></svg>
+                </button>
             </div>
 
             
             <div>
-            <h2>Hi {siteUser.username}!</h2>
-            {/* <UserContext.Provider value={ authUserData }> */}
-            <UserContext.Provider value={ siteUser }>
-                <Router>
-                    <Suspense fallback={<p>Loading...</p>}>
-                        <Routes>
-                            <Route path={RoutePaths.LOGIN} element={<Login />} />
-                            <Route path={RoutePaths.PROFILE} element={<Profile user={user} />} />
-                            <Route exact path={RoutePaths.DASHBOARD} element={<Dashboard />} />
-                            <Route exact path={RoutePaths.CAROUSELS} element={<Carousels />} />
-                            <Route exact path={RoutePaths.DRAGANDDROP} element={<DragAndDrop />} />
-                            <Route exact path={RoutePaths.FIREBASE_TEST} element={<FIREBASE_TEST />} />
-                            <Route exact path={RoutePaths.WILLIAM} element={<William />} />
-                            <Route exact path={RoutePaths.TAILWIND} element={<Tailwind />} />
-                            <Route exact path={RoutePaths.TAILWIND_EXAMPLE} element={<Tailwind_Example />} />
-                            <Route exact path={RoutePaths.SOCIAL_DASHBOARD} element={<Social_Dashboard />} />
-                            <Route exact path={RoutePaths.SOCIAL_LOGIN} element={<Social_Login />} />
-                            <Route exact path={RoutePaths.SOCIAL_SIGN_UP} element={<Social_Signup />} />
-                            <Route exact path={RoutePaths.SOCIAL_PROFILE} element={<Social_Profile />} />
-                            <Route path="*" element={<NotFound/>} />
-                        </Routes>
-                    </Suspense>
-                </Router>
+                <h2>Hi {siteUser.username}!</h2>
+                <UserContext.Provider value={ siteUser }>
+                    <Router>
+                        <Suspense fallback={<p>Loading...</p>}>
+                            <Routes>
+                                <Route path={RoutePaths.LOGIN} element={<Login />} />
+                                <Route path={RoutePaths.PROFILE} element={<Profile user={user} />} />
+                                <Route exact path={RoutePaths.DASHBOARD} element={<Dashboard />} />
+                                <Route exact path={RoutePaths.CAROUSELS} element={<Carousels />} />
+                                <Route exact path={RoutePaths.DRAGANDDROP} element={<DragAndDrop />} />
+                                <Route exact path={RoutePaths.FIREBASE_TEST} element={<FIREBASE_TEST />} />
+                                <Route exact path={RoutePaths.WILLIAM} element={<William />} />
+                                <Route exact path={RoutePaths.TAILWIND} element={<Tailwind />} />
+                                <Route exact path={RoutePaths.TAILWIND_EXAMPLE} element={<Tailwind_Example />} />
+                                <Route exact path={RoutePaths.SOCIAL_DASHBOARD} element={<Social_Dashboard />} />
+                                <Route exact path={RoutePaths.SOCIAL_LOGIN} element={<Social_Login />} />
+                                <Route exact path={RoutePaths.SOCIAL_SIGN_UP} element={<Social_Signup />} />
+                                <Route exact path={RoutePaths.SOCIAL_PROFILE} element={<Social_Profile />} />
+                                <Route path="*" element={<NotFound/>} />
+                            </Routes>
+                        </Suspense>
+                    </Router>
 
-            </UserContext.Provider>
-                </div>
-            <div class="fixed inset-x-0 bottom-0 bg-gray-500 py-5 mt-12 w-full flex flex-col items-center border-t-2 border-gray-600 text-white shadow-lg">
-                Copyright © 2022
-
-            
-            
-            <div className="absolute bottom-0 left-0 w-15 h-5 p-5">
-                <p className="text-xs text-teal-400">
-                    v7.7
-                </p>
+                </UserContext.Provider>
             </div>
+            <div class="bg-gray-500 py-5 mt-12 w-full flex flex-col items-center border-t-2 border-gray-600 text-white shadow-lg">
+                Copyright © 2022
+                <div className="absolute bottom-0 left-0 w-15 h-5 p-5">
+                    <p className="text-xs text-teal-400">
+                        v7.7.2
+                    </p>
+                </div>
             </div>
 
         </div>
