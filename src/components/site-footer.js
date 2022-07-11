27bcf192e1159d92305da5c63bd2f33449e4a972
Brendan Loder <brendan.loder@gmail.com>
2022-07-11 13:41:04 -0400

    v7.9.1 starting dropdown

--- src/components/site-footer.js
+++ src/components/site-footer.js
@@ -1,15 +1,16 @@
 export default function SiteFooter() {
     return (
 
-        <div class="bg-gray-500 py-5 px-10 mt-2 w-full flex flex-col items-center border-t-2 border-gray-600 text-white relative">
+        <div class="bg-gray-500 py-5 px-10 mt-2 w-full flex flex-col items-center border-t-2 border-gray-600 text-white relative z-0">
 
             Copyright © 2022<br/>
 
             <div className="absolute bottom-0 left-0 w-15 h-5 p-5">
                 <p className="text-xs text-teal-400">
-                    v7.9.0
+                    v7.9.1
                 </p>
             </div>
+            
             
         </div>
 
--- src/components/site-header.js
+++ src/components/site-header.js
@@ -1,15 +1,31 @@
 export default function SiteHeader() {
     return (
-        <div className="bg-gray-500 flex justify-center text-5xl w-full font-bold text-white border-b-2 border-gray-600 relative
-            ">
+        <div className="bg-gray-500 flex justify-center text-5xl w-full font-bold text-white border-b-2 border-gray-600 relative">
                 <div class="p-3 font-serif text-2xl border border-1 m-2 font-semibold">
                     Three Grays
                 </div>
 
-                <div className="absolute top-3 right-3">
-                    <button type="button" class="text-white hover:text-gray-300">
+                <div className="absolute top-5 right-10 z-40 font-serif text-sm">
+
+
+                
+<div class="dropdown inline-block relative">
+  <button class="bg-gray-300 text-gray-700 font-semibold py-2 px-4 rounded inline-flex items-center">
+  <svg class="w-4 h-4 fill-current" viewBox="0 0 24 24"><path fill-rule="evenodd" d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"/></svg>
+    
+  </button>
+  <ul class="dropdown-menu absolute hidden text-gray-700 pt-1 pr-4">
+    <li class=""><a class="rounded-t bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap" href="#">One</a></li>
+    <li class=""><a class="bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap" href="#">Two</a></li>
+    <li class=""><a class="rounded-b bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap" href="#">Three is the magic number</a></li>
+  </ul>
+</div>
+
+
+                    
+                    {/* <button type="button" class="text-white hover:text-gray-300">
                         <svg class="w-9 h-9 fill-current" viewBox="0 0 24 24"><path fill-rule="evenodd" d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"/></svg>
-                    </button>
+                    </button> */}
                 </div>
             </div>
     )
