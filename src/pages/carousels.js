15d9989fd8b37a57f8c4de573bbdde2fbe44d204
Brendan Loder <brendan.loder@gmail.com>
2022-08-12 18:02:28 -0400

    8.1 Swapped out carousels for tailwind-elements 2

--- src/pages/carousels.js
+++ src/pages/carousels.js
@@ -1,76 +1,78 @@
+import { useEffect } from "react";
 
 function Carousels() {
+
+useEffect(() => {
+        
+    document.title = `Carousels`;
+}, [])
     return (
         <div className="p-5">
-            <div
-  id="carouselExampleCrossfade"
-  class="carousel slide carousel-fade relative w-60 m-auto"
-  data-bs-ride="carousel"
->
-  <div class="carousel-indicators absolute right-0 bottom-0 left-0 flex justify-center p-0 mb-4">
-    <button
-      type="button"
-      data-bs-target="#carouselExampleCrossfade"
-      data-bs-slide-to="0"
-      class="active"
-      aria-current="true"
-      aria-label="Slide 1"
-    ></button>
-    <button
-      type="button"
-      data-bs-target="#carouselExampleCrossfade"
-      data-bs-slide-to="1"
-      aria-label="Slide 2"
-    ></button>
-    <button
-      type="button"
-      data-bs-target="#carouselExampleCrossfade"
-      data-bs-slide-to="2"
-      aria-label="Slide 3"
-    ></button>
-  </div>
-  <div class="carousel-inner relative w-full overflow-hidden">
-    <div class="carousel-item active float-left w-full">
-      <img
-        src="/images/t2.jpg"
-        class="block w-full"
-        alt="Wild Landscape"
-      />
-    </div>
-    <div class="carousel-item float-left w-full">
-      <img
-        src="/images/t3.jpg"
-        class="block w-full"
-        alt="Camera"
-      />
-    </div>
-    <div class="carousel-item float-left w-full">
-      <img
-        src="/images/t6.jpg"
-        class="block w-full"
-        alt="Exotic Fruits"
-      />
-    </div>
-  </div>
-  <button
-    class="carousel-control-prev absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline left-0"
-    type="button"
-    data-bs-target="#carouselExampleCrossfade"
-    data-bs-slide="prev"
-  >
-    <span class="carousel-control-prev-icon inline-block bg-no-repeat" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button
-    class="carousel-control-next absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline right-0"
-    type="button"
-    data-bs-target="#carouselExampleCrossfade"
-    data-bs-slide="next"
-  >
-    <span class="carousel-control-next-icon inline-block bg-no-repeat" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-</div>
+            <div id="carouselExampleCrossfade" class="carousel slide carousel-fade relative w-60 m-auto" data-bs-ride="carousel">
+                <div class="carousel-indicators absolute right-0 bottom-0 left-0 flex justify-center p-0 mb-4">
+                    <button
+                    type="button"
+                    data-bs-target="#carouselExampleCrossfade"
+                    data-bs-slide-to="0"
+                    class="active"
+                    aria-current="true"
+                    aria-label="Slide 1"
+                    ></button>
+                    <button
+                    type="button"
+                    data-bs-target="#carouselExampleCrossfade"
+                    data-bs-slide-to="1"
+                    aria-label="Slide 2"
+                    ></button>
+                    <button
+                    type="button"
+                    data-bs-target="#carouselExampleCrossfade"
+                    data-bs-slide-to="2"
+                    aria-label="Slide 3"
+                    ></button>
+                </div>
+                <div class="carousel-inner relative w-full overflow-hidden">
+                    <div class="carousel-item active float-left w-full">
+                    <img
+                        src="/images/t2.jpg"
+                        class="block w-full"
+                        alt="Wild Landscape"
+                    />
+                    </div>
+                    <div class="carousel-item float-left w-full">
+                    <img
+                        src="/images/t3.jpg"
+                        class="block w-full"
+                        alt="Camera"
+                    />
+                    </div>
+                    <div class="carousel-item float-left w-full">
+                    <img
+                        src="/images/t6.jpg"
+                        class="block w-full"
+                        alt="Exotic Fruits"
+                    />
+                    </div>
+                </div>
+                <button
+                    class="carousel-control-prev absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline left-0"
+                    type="button"
+                    data-bs-target="#carouselExampleCrossfade"
+                    data-bs-slide="prev"
+                >
+                    <span class="carousel-control-prev-icon inline-block bg-no-repeat" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button
+                    class="carousel-control-next absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline right-0"
+                    type="button"
+                    data-bs-target="#carouselExampleCrossfade"
+                    data-bs-slide="next"
+                >
+                    <span class="carousel-control-next-icon inline-block bg-no-repeat" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
         </div>
     )
 }
