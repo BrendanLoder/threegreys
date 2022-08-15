9a9e4982a9b0cb2ac6da65a890cbd3bea0830e4c
Brendan Loder <brendan.loder@gmail.com>
2022-08-15 17:44:10 -0400

    version 8.5 tiny edit on duplicate do not wants on dashboard

--- src/social/pages/dashboard.js
+++ src/social/pages/dashboard.js
@@ -230,8 +230,6 @@ export default function Dashboard() {
                         <div id="doNotWantsCollapseOne" className="accordion-collapse collapse" aria-labelledby="doNotWantsHeadingOne">
                             <div className="accordion-body py-4 px-5 max-h-48  overflow-scroll no-scrollbar">
                                 {doNotWantItems}
-                                {doNotWantItems}
-                                {doNotWantItems}
                             </div>
                         </div>
                     </div>
