730c27abb6d7855558ecadac47016993420140b6
Brendan Loder <brendan.loder@gmail.com>
2022-10-07 10:58:33 -0400

    version 9.8.1 -- comparing carousels to fix spacing

--- src/social/components/spotlight.js
+++ src/social/components/spotlight.js
@@ -80,12 +80,103 @@ export default function Spotlight({
     }, [])
 
     return (
-        <div className='bg-green-100'>
+        <div className='bg-yellow-100 h-[900px]'>
             {console.log('inline slides:',slides)}
-            
-            {slideButtons}
 
-            <div id="spotlightCarousel" className="carousel slide carousel-fade relative w-60 m-auto bg-blue-100" data-bs-ride="carousel"  data-bs-interval="false">
+
+            <div id="spotlightCarousel" className="carousel slide carousel-fade relative w-60 m-auto" data-bs-ride="carousel"  data-bs-interval="false">
+                <div className="carousel-indicators absolute right-0 bottom-0 left-0 flex justify-center p-0 mb-4">
+                {slideButtons}
+                </div>
+                <div className="carousel-inner relative w-full overflow-hidden">
+                {slides}
+                </div>
+                <button
+                    className="carousel-control-prev absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline left-0"
+                    type="button"
+                    data-bs-target="#spotlightCarousel"
+                    data-bs-slide="prev"
+                >
+                    <span className="carousel-control-prev-icon inline-block bg-no-repeat" aria-hidden="true"></span>
+                    <span className="visually-hidden">Previous</span>
+                </button>
+                <button
+                    className="carousel-control-next absolute top-0 bottom-0 flex items-kcenter justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline right-0"
+                    type="button"
+                    data-bs-target="#spotlightCarousel"
+                    data-bs-slide="next"
+                >
+                    <span className="carousel-control-next-icon inline-block bg-no-repeat" aria-hidden="true"></span>
+                    <span className="visually-hidden">Next</span>
+                </button>
+            </div>
+            <br/><br/>
+            <div id="carouselExampleCrossfade" className="carousel slide carousel-fade relative w-60 m-auto" data-bs-ride="carousel"  data-bs-interval="false">
+                <div className="carousel-indicators absolute right-0 bottom-0 left-0 flex justify-center p-0 mb-4">
+                    <button
+                    type="button"
+                    data-bs-target="#carouselExampleCrossfade"
+                    data-bs-slide-to="0"
+                    className="active"
+                    aria-current="true"
+                    aria-label="Slide 1"
+                    ></button>
+                    <button
+                    type="button"
+                    data-bs-target="#carouselExampleCrossfade"
+                    data-bs-slide-to="1"
+                    aria-label="Slide 2"
+                    ></button>
+                    <button
+                    type="button"
+                    data-bs-target="#carouselExampleCrossfade"
+                    data-bs-slide-to="2"
+                    aria-label="Slide 3"
+                    ></button>
+                </div>
+                <div className="carousel-inner relative w-full overflow-hidden">
+                    <div className="carousel-item active float-left w-full">
+                    <img
+                        src="/images/t2.jpg"
+                        className="block w-full"
+                        alt="Wild Landscape"
+                    />
+                    </div>
+                    <div className="carousel-item float-left w-full">
+                    <img
+                        src="/images/t3.jpg"
+                        className="block w-full"
+                        alt="Camera"
+                    />
+                    </div>
+                    <div className="carousel-item float-left w-full">
+                    <img
+                        src="/images/t6.jpg"
+                        className="block w-full"
+                        alt="Exotic Fruits"
+                    />
+                    </div>
+                </div>
+                <button
+                    className="carousel-control-prev absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline left-0"
+                    type="button"
+                    data-bs-target="#carouselExampleCrossfade"
+                    data-bs-slide="prev"
+                >
+                    <span className="carousel-control-prev-icon inline-block bg-no-repeat" aria-hidden="true"></span>
+                    <span className="visually-hidden">Previous</span>
+                </button>
+                <button
+                    className="carousel-control-next absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline right-0"
+                    type="button"
+                    data-bs-target="#carouselExampleCrossfade"
+                    data-bs-slide="next"
+                >
+                    <span className="carousel-control-next-icon inline-block bg-no-repeat" aria-hidden="true"></span>
+                    <span className="visually-hidden">Next</span>
+                </button>
+            </div>
+            {/* <div id="spotlightCarousel" className="carousel slide carousel-fade relative w-96 m-auto h-52 bg-blue-100" data-bs-ride="carousel"  data-bs-interval="false">
                 <div className="carousel-indicators absolute right-0 bottom-0 left-0 flex justify-center p-0 mb-4 bg-orange-100">
                     {slideButtons}
                 </div>
@@ -110,7 +201,7 @@ export default function Spotlight({
                     <span className="carousel-control-next-icon inline-block bg-no-repeat" aria-hidden="true"></span>
                     <span className="visually-hidden">Next</span>
                 </button>
-            </div>
+            </div> */}
 
         </div>
     )
