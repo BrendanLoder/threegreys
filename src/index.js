cc4c2623c7f7c3db0b067fdae7f4746e6e1cfbea
root <root@ViolentSociety>
2024-09-28 20:51:35 -0400

    removed added node engine directive in packages.json

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Amplify from 'aws-amplify';
+/*import Amplify from 'aws-amplify';*/
+import { Amplify } from 'aws-amplify'
 import config from './aws-exports';
 import { firebase, db } from './lib/firebase'
 import FirebaseContext from './context/firebase'
