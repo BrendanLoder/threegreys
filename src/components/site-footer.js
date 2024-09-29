dfd1eeffad5508db4e6d04395956ed61c9eb819f
root <root@ViolentSociety>
2024-09-29 14:26:32 -0400

    Added node version to packages.json but still overriding it in amplify

--- src/components/site-footer.js
+++ src/components/site-footer.js
@@ -8,7 +8,7 @@ export default function SiteFooter() {
 
             <div className="absolute bottom-0 left-0 w-15 h-5 p-5">
                 <p className="text-xs text-teal-400">
-                    v10.1.0
+                    v10.2.0
                 </p>
             </div>
               
